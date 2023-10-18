3663c218510deabe3a0524cc83dac42fc65fb2d0
Josue Molina <Josue11183@byui.edu>
2023-10-18 10:17:20 -0600

    Getting to work with SQLite

--- movies.py
+++ movies.py
@@ -1,1 +1,16 @@
 import sqlite3
+
+def create_connection():
+    #Connection object
+    connect_obj = sqlite3.connect("Movies")
+    # Cursor Object 
+    cursor_obj = connect_obj.cursor()
+    # Create table
+    table = """CREATE table MOVIES (
+               MOVIE VARCHAR(255), 
+               YEAR INT(4),
+               GENRE CHARO(25)
+             );
+            """
+
+print(table)
