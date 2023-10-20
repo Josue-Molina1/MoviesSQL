99e97536ba79dcd03d46217e4b21e56a344d7eb5
Josue Molina <Josue11183@byui.edu>
2023-10-19 21:49:53 -0600

    document duh

--- movies.py
+++ movies.py
@@ -1,7 +1,7 @@
 import sqlite3
-
+# Connection between python and the database (create a .db file)
 conn = sqlite3.connect('movies.db')
-
+# This allows me to execute the statements. 
 cursor = conn.cursor()
 
 # cursor.execute("""CREATE TABLE movies(
@@ -17,9 +17,15 @@ cursor = conn.cursor()
 
 # cursor.execute("INSERT INTO movies VALUES ('Whispers of the Heart0, '1995', 'Romance', 'PG', '2', 'Best movie ever', 'Issei Takahashi', '34.9' ')")
 
-cursor.execute("SELECT * FROM movies")
-print(cursor.fetchall)
 
+# Basic SELECT statement, one line only. 
+cursor.execute("SELECT * FROM movies")
+print(cursor.fetchall) # Grab everything from that query. 
+
+# I dunno know what this is, I was told to put it there, I'll figure it our. 
 conn.commit()
 
+# Important. 
 conn.close()
+
+# Here
