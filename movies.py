23a71d85dbfec50bb11fff9977ddd8924b5c318c
Josue Molina <Josue11183@byui.edu>
2023-10-19 21:43:53 -0600

    enough for today

--- movies.py
+++ movies.py
@@ -1,2 +1,25 @@
 import sqlite3
-connect_obj = sqlite3.connect('movies.db')
+
+conn = sqlite3.connect('movies.db')
+
+cursor = conn.cursor()
+
+# cursor.execute("""CREATE TABLE movies(
+#                name text,
+#                date integer,
+#                genre text,
+#                rate text,
+#                duration integer,
+#                overview text, 
+#                cast text,
+#                revenue integer
+#                )""")
+
+# cursor.execute("INSERT INTO movies VALUES ('Whispers of the Heart0, '1995', 'Romance', 'PG', '2', 'Best movie ever', 'Issei Takahashi', '34.9' ')")
+
+cursor.execute("SELECT * FROM movies")
+print(cursor.fetchall)
+
+conn.commit()
+
+conn.close()
